bf6bb3e62e63a789e960b2a32e814c867036960f
shadelbereth <scallakim@hotmail.com>
2017-08-07 09:40:26 +0200

    Create project + rocket basic moves

---
+++ Assets/Scripts/RocketMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMove : MonoBehaviour {
+
+    Rigidbody2D m_rigidbody;
+    [SerializeField]
+    float initialSpeed = 100;
+    [SerializeField]
+    float speedLoss = 5;
+    [SerializeField]
+    float fuel = 90;
+    [SerializeField]
+    float speedBoost = 10;
+    [SerializeField]
+    float fuelUsedByBoost = 10;
+
+    public delegate void OnFuelLevelChanged(float fuel);
+    public event OnFuelLevelChanged FuelLevelChange;
+
+	// Use this for initialization
+	void Start () {
+        Physics2D.gravity = Vector3.zero;
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        m_rigidbody.AddForce(transform.up * initialSpeed);
+        if (FuelLevelChange != null)
+        {
+            FuelLevelChange.Invoke(fuel);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(m_rigidbody.velocity.magnitude);
+        if (m_rigidbody.velocity.magnitude > 0.01)
+            m_rigidbody.AddForce(transform.up * (-1) * speedLoss * Time.deltaTime);
+    }
+
+    public void Boost ()
+    {
+        if (fuel >= fuelUsedByBoost)
+        {
+            m_rigidbody.AddForce(transform.up * speedBoost);
+            fuel -= fuelUsedByBoost;
+            if (FuelLevelChange != null)
+            {
+                FuelLevelChange.Invoke(fuel);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateFuelUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateFuelUI : MonoBehaviour {
+
+    Text fuelUI;
+
+	// Use this for initialization
+	void Start () {
+        fuelUI = GetComponent<Text>();
+	}
+
+    private void OnEnable()
+    {
+        RocketMove player = GameObject.FindGameObjectWithTag("Player").GetComponent<RocketMove>();
+        player.FuelLevelChange += UpdateUI;
+    }
+
+    private void OnDisable()
+    {
+        RocketMove player = GameObject.FindGameObjectWithTag("Player").GetComponent<RocketMove>();
+        player.FuelLevelChange -= UpdateUI;
+    }
+
+    public void UpdateUI(float fuelLevel)
+    {
+        fuelUI.text = fuelLevel.ToString();
+    }
+}
