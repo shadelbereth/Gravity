a23605f6611fe9178ffcb302e0c19c9bc76cc724
shadelbereth <scallakim@hotmail.com>
2017-08-13 12:33:42 +0200

    Camera test

---
+++ Assets/Scripts/CameraFollows.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float smoothing = 5f;
+
+    void Start()
+    {
+        Vector3 targetPos = target.position;
+        targetPos.z = transform.position.z;
+        transform.position = targetPos;
+    }
+
+    void FixedUpdate () {
+        Vector3 targetPos = target.position;
+        targetPos.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothing * Time.fixedDeltaTime);
+	}
+}
