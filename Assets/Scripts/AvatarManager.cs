8700788317e2eb97c11a1c5f3625b3971bd68ede
shadelbereth <scallakim@hotmail.com>
2017-08-13 18:32:12 +0200

    Avatar picker system

---
+++ Assets/Scripts/AvatarManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarManager : MonoBehaviour {
+
+    SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+        UpdateAvatarColor();
+	}
+
+    public void UpdateAvatarColor ()
+    {
+        if (PlayerPrefs.HasKey("AvatarColorRed") && PlayerPrefs.HasKey("AvatarColorGreen") && PlayerPrefs.HasKey("AvatarColorBlue"))
+        {
+            sprite.color = new Color(PlayerPrefs.GetFloat("AvatarColorRed"), PlayerPrefs.GetFloat("AvatarColorGreen"), PlayerPrefs.GetFloat("AvatarColorBlue"));
+        }
+    }
+}
---
+++ Assets/Scripts/AvatarPicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarPicker : MonoBehaviour {
+
+    [SerializeField]
+    Color color = Color.white;
+
+    public delegate void OnAvatarColorChange();
+    public event OnAvatarColorChange AvatarColorChanged;
+
+    private void Start()
+    {
+        Button button = GetComponent<Button>();
+        ColorBlock newTransition = button.colors;
+        newTransition.normalColor = color;
+        newTransition.highlightedColor = new Color(color.r - 10f / 255f, color.g - 10f / 255f, color.b - 10f / 255f);
+        newTransition.pressedColor = new Color(color.r - 55f / 255f, color.g - 55f / 255f, color.b - 55f / 255f);
+        newTransition.disabledColor = new Color(color.r - 55f / 255f, color.g - 55f / 255f, color.b - 55f / 255f, color.a / 2);
+        button.colors = newTransition;
+        button.onClick.AddListener(ChangeAvatarColor);
+        AvatarColorChanged += GameObject.FindGameObjectWithTag("Player").GetComponent<AvatarManager>().UpdateAvatarColor;
+    }
+
+    public void ChangeAvatarColor ()
+    {
+        PlayerPrefs.SetFloat("AvatarColorRed", color.r);
+        PlayerPrefs.SetFloat("AvatarColorGreen", color.g);
+        PlayerPrefs.SetFloat("AvatarColorBlue", color.b);
+        AvatarColorChanged.Invoke();
+    }
+}
