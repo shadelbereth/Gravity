951770e721ed1089647173d201f07a24a9f2fe70
shadelbereth <scallakim@hotmail.com>
2017-08-13 19:44:24 +0200

    Message change if no name

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -7,18 +7,32 @@ using UnityEngine.SceneManagement;
 public class StartManager : MonoBehaviour {
 
     InputField playerName;
+    Text placeholder;
     [SerializeField]
     string nextScene = "Test";
+    [SerializeField]
+    List<string> placeHolderTexts;
+    int count;
 
     private void Start()
     {
         playerName = GameObject.FindGameObjectWithTag("PlayerName").GetComponent<InputField>();
+        placeholder = GameObject.FindGameObjectWithTag("Placeholder").GetComponent<Text>();
+        count = 0;
+        placeholder.text = placeHolderTexts[count];
+        PlayerPrefs.DeleteKey("PlayerName");
+        if (PlayerPrefs.HasKey("PlayerName"))
+        {
+            playerName.text = PlayerPrefs.GetString("PlayerName");
+        }
     }
 
     public void ValidateStartInfo ()
     {
         if (playerName.text.Equals(""))
         {
+            count++;
+            placeholder.text = placeHolderTexts[count % placeHolderTexts.Count];
             return;
         }
         PlayerPrefs.SetString("PlayerName", playerName.text);
