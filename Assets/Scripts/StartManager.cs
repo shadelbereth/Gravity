2032fcf571567e816aa079c8dc9eef3c2e586d5e
shadelbereth <scallakim@hotmail.com>
2017-08-13 19:00:50 +0200

    Start info validation + scene change

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour {
+
+    InputField playerName;
+    [SerializeField]
+    string nextScene = "Test";
+
+    private void Start()
+    {
+        playerName = GameObject.FindGameObjectWithTag("PlayerName").GetComponent<InputField>();
+    }
+
+    public void ValidateStartInfo ()
+    {
+        if (playerName.text.Equals(""))
+        {
+            return;
+        }
+        PlayerPrefs.SetString("PlayerName", playerName.text);
+        SceneManager.LoadScene(nextScene);
+    }
+}
